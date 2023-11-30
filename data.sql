a04f07c9957b800bb0319a1d90c0e0eb72568766
Lellalu <lella.lu14@outlook.com>
2023-11-30 23:30:35 +0100

    Update seminar 2

--- data.sql
+++ data.sql
@@ -1,4 +1,4 @@
-INSERT INTO person (id,personnumber,name,city,zip,street)
+INSERT INTO person (id,person_number,name,city,zip,street)
 VALUES
   (1,'019860536304','Yardley Clark','Waiuku','23606','897-1343 Orci, St.'),
   (2,'113883285512','Fitzgerald Burton','Queanbeyan','72274','Ap #715-5474 Pede St.'),
@@ -11,7 +11,7 @@ VALUES
   (9,'474684596367','Lucy Chandler','Chemnitz','45392','Ap #881-2203 Consequat Avenue'),
   (10,'188243537164','Holly Ochoa','Joinville','36227','Ap #227-3390 In St.');
 
-INSERT INTO lessonprice (id,type,level,priceamount,starttime,endtime)
+INSERT INTO lesson_price (id,type,level,price_amount,start_time,end_time)
 VALUES
   (1,'individual','beginner',517,'Nov 4, 2022','Dec 18, 2022'),
   (2,'individual','beginner',512,'Nov 19, 2022','Dec 22, 2022'),
@@ -24,20 +24,20 @@ VALUES
   (9,'ensemble','advanced',687,'Nov 4, 2022','Dec 11, 2022'),
   (10,'individual','beginner',697,'Nov 21, 2022','Dec 12, 2022');
 
-INSERT INTO instrument (id,brand,type,quantity)
+INSERT INTO instrument_stock (id,brand,type,quantity,rented_price_per_month)
 VALUES
-  (1,'Curran','Caleb',8),
-  (2,'Duncan','Yoko',10),
-  (3,'Basil','Garrison',3),
-  (4,'Maryam','Ferdinand',1),
-  (5,'Yuli','Reuben',9),
-  (6,'Barclay','Carlos',2),
-  (7,'Ethan','Nelle',0),
-  (8,'Tamara','Elliott',4),
-  (9,'Lillian','Isabelle',9),
-  (10,'Kylynn','Phelan',2);
+  (1,'Curran','Caleb',8,10),
+  (2,'Duncan','Yoko',10,12),
+  (3,'Basil','Garrison',3,15),
+  (4,'Maryam','Ferdinand',1,17),
+  (5,'Yuli','Reuben',9,20),
+  (6,'Barclay','Carlos',2,18),
+  (7,'Ethan','Nelle',0,11),
+  (8,'Tamara','Elliott',4,13),
+  (9,'Lillian','Isabelle',9,14),
+  (10,'Kylynn','Phelan',2,16);
 
-INSERT INTO instructor (id,instrumentstaught,person_id)
+INSERT INTO instructor (id,instruments_taught,person_id)
 VALUES
   (1,'suscipit',1),
   (2,'enim',2),
@@ -50,33 +50,40 @@ VALUES
   (9,'Nullam',9),
   (10,'metus.',10);
 
-INSERT INTO student (id,discount,siblinginfo,person_id,contactpersonphone)
+INSERT INTO student (id,person_id,contact_person_phone)
 VALUES
-  (1,0.8,'ultricies ornare, elit elit fermentum risus,',1,'700142335194'),
-  (2,0.8,'orci quis',2,'862816876623'),
-  (3,1,'dapibus quam quis diam. Pellentesque',3,'834066242681'),
-  (4,1,'et arcu imperdiet ullamcorper. Duis at lacus.',4,'901727622753'),
-  (5,0.8,'porttitor eros nec tellus.',5,'306441459681'),
-  (6,0.8,'',6,'738341901265'),
-  (7,1,'nec ligula consectetuer rhoncus.',7,'216497866266'),
-  (8,1,'Phasellus in',8,'624667284758'),
-  (9,0.8,'vel, vulputate eu,',9,'076570255459'),
-  (10,0.8,'fermentum risus, at fringilla purus mauris a nunc. In',10,'406964456164');
+  (1,1,'700142335194'),
+  (2,2,'862816876623'),
+  (3,3,'834066242681'),
+  (4,4,'901727622753'),
+  (5,5,'306441459681'),
+  (6,6,'738341901265'),
+  (7,7,'216497866266'),
+  (8,8,'624667284758'),
+  (9,9,'076570255459'),
+  (10,10,'406964456164');
 
-INSERT INTO lessonschedule (id,starttime,endtime,lessonpriceid,instructor_id)
+INSERT INTO sibling (student_id,sibling_id)
 VALUES
-  (1,'Nov 8, 2022','Dec 20, 2022',7,3),
-  (2,'Nov 18, 2022','Dec 15, 2022',8,7),
-  (3,'Nov 19, 2022','Dec 31, 2022',6,7),
-  (4,'Nov 9, 2022','Dec 30, 2022',5,7),
-  (5,'Nov 9, 2022','Dec 31, 2022',5,4),
-  (6,'Nov 20, 2022','Dec 6, 2022',5,5),
-  (7,'Nov 9, 2022','Dec 10, 2022',3,9),
-  (8,'Nov 12, 2022','Dec 8, 2022',6,7),
-  (9,'Nov 12, 2022','Dec 19, 2022',2,4),
-  (10,'Nov 7, 2022','Dec 16, 2022',7,2);
+  (1,2),
+  (1,3),
+  (4,5),
+  (7,8);
 
-INSERT INTO lessoncapacity (id,max,min,lessonid)
+INSERT INTO lesson_schedule (id,start_time,end_time,instructor_id,price_id)
+VALUES
+  (1,'Jan 12, 2022','Aug 31, 2022',1,1),
+  (2,'Mar 5, 2022','Jul 24, 2022',2,2),
+  (3,'Jun 20, 2022','Sep 7, 2022',3,3),
+  (4,'Jan 23, 2022','Jul 16, 2022',4,4),
+  (5,'May 14, 2022','Jul 2, 2022',5,5),
+  (6,'Mar 7, 2022','Nov 28, 2022',6,6),
+  (7,'Feb 16, 2022','Nov 6, 2022',7,7),
+  (8,'Jun 29, 2022','Dec 21, 2022',8,8),
+  (9,'Apr 1, 2022','Aug 23, 2022',9,9),
+  (10,'Apr 5, 2022','Sep 27, 2022',10,10);
+
+INSERT INTO lesson_capacity (id,max,min,lesson_id)
 VALUES
   (1,137,9,1),
   (2,139,9,2),
@@ -89,7 +96,7 @@ VALUES
   (9,105,6,9),
   (10,186,4,10);
 
-INSERT INTO lessongenre (id,genre,lessonid)
+INSERT INTO lesson_genre (id,genre,lesson_id)
 VALUES
   (1,'amet',1),
   (2,'mauris',2),
@@ -102,7 +109,7 @@ VALUES
   (9,'orci,',9),
   (10,'aliquam',10);
 
-INSERT INTO lessoninstrument (id,taughtinstrument,lessonid)
+INSERT INTO lesson_instrument (id,taught_instrument,lesson_id)
 VALUES
   (1,'ridiculus',1),
   (2,'in',2),
@@ -115,7 +122,7 @@ VALUES
   (9,'tincidunt',9),
   (10,'Integer',10);
 
-INSERT INTO phone (id,phoneno)
+INSERT INTO phone (id,phone_no)
 VALUES
   (1,'455839225514'),
   (2,'433653452749'),
@@ -141,7 +148,7 @@ VALUES
   (9,9),
   (10,10);
 
-INSERT INTO student_lessonschedule (lessonid,student_id)
+INSERT INTO student_lesson_schedule (lesson_id,student_id)
 VALUES
   (1,1),
   (2,2),
@@ -154,7 +161,7 @@ VALUES
   (9,9),
   (10,10);
 
-INSERT INTO rental (id,startdate,enddate,instrument_id,student_id)
+INSERT INTO rental (id,start_date,end_date,instrument_id,student_id)
 VALUES
   (1,'Nov 12, 2022','Dec 28, 2022',1,1),
   (2,'Nov 24, 2022','Dec 20, 2022',2,2),
--- soundGood.sql
+++ soundGood.sql
@@ -1,162 +1,173 @@
-CREATE TYPE lessonType AS ENUM ('individual', 'group', 'ensemble');
-CREATE TYPE lessonLevel AS ENUM ('beginner', 'intermediate', 'advanced');
+CREATE TYPE LESSONTYPE AS ENUM ('individual', 'group', 'ensemble');
+CREATE TYPE LESSONLEVEL AS ENUM ('beginner', 'intermediate', 'advanced');
 
-CREATE TABLE Instrument (
- id INT NOT NULL,
- brand VARCHAR(10) NOT NULL,
- type VARCHAR(10) NOT NULL,
- quantity VARCHAR(10) NOT NULL
+
+CREATE TABLE instrument_stock (
+id INT NOT NULL,
+brand VARCHAR(10) NOT NULL,
+type VARCHAR(10) NOT NULL,
+quantity VARCHAR(10) NOT NULL,
+rented_price_per_month VARCHAR(10) NOT NULL
 );
 
-ALTER TABLE Instrument ADD CONSTRAINT PK_Instrument PRIMARY KEY (id);
+ALTER TABLE instrument_stock ADD CONSTRAINT PK_instrument_stock PRIMARY KEY (id);
 
 
-CREATE TABLE LessonPrice (
- id INT NOT NULL,
- type lessonType NOT NULL,
- level  lessonLevel NOT NULL,
- priceAmount  VARCHAR(10) NOT NULL,
- startTime TIMESTAMP NOT NULL,
- endTime TIMESTAMP
+CREATE TABLE lesson_price (
+id INT NOT NULL,
+price_amount  VARCHAR(10) NOT NULL,
+start_time TIMESTAMP NOT NULL,
+end_time TIMESTAMP,
+type LESSONTYPE NOT NULL,
+level LESSONLEVEL NOT NULL
 );
 
-ALTER TABLE LessonPrice ADD CONSTRAINT PK_LessonPrice PRIMARY KEY (id);
+ALTER TABLE lesson_price ADD CONSTRAINT PK_lesson_price PRIMARY KEY (id);
 
 
-CREATE TABLE Person (
- id INT NOT NULL,
- personNumber VARCHAR(12) NOT NULL UNIQUE,
- name CHAR(100) NOT NULL,
- city VARCHAR(50),
- zip VARCHAR(5),
- street VARCHAR(100)
+CREATE TABLE person (
+id INT NOT NULL,
+person_number VARCHAR(12) NOT NULL,
+name VARCHAR(100) NOT NULL,
+city VARCHAR(50),
+zip VARCHAR(5),
+street VARCHAR(100)
 );
 
-ALTER TABLE Person ADD CONSTRAINT PK_Person PRIMARY KEY (id);
+ALTER TABLE person ADD CONSTRAINT PK_person PRIMARY KEY (id);
 
 
-CREATE TABLE Phone (
- id INT NOT NULL,
- phoneNo VARCHAR(12) UNIQUE
+CREATE TABLE phone (
+id INT NOT NULL,
+phone_no VARCHAR(12) NOT NULL
 );
 
-ALTER TABLE Phone ADD CONSTRAINT PK_Phone PRIMARY KEY (id);
+ALTER TABLE phone ADD CONSTRAINT PK_phone PRIMARY KEY (id);
 
 
-CREATE TABLE Student (
- id INT NOT NULL,
- discount VARCHAR(10),
- siblingInfo VARCHAR(500),
- person_id INT NOT NULL,
- contactPersonPhone VARCHAR(12)
+CREATE TABLE student (
+id INT NOT NULL,
+person_id INT NOT NULL,
+contact_person_phone VARCHAR(12)
 );
 
-ALTER TABLE Student ADD CONSTRAINT PK_Student PRIMARY KEY (id);
+ALTER TABLE student ADD CONSTRAINT PK_student PRIMARY KEY (id);
 
 
-CREATE TABLE Instructor (
- id INT NOT NULL,
- instrumentsTaught CHAR(500) NOT NULL,
- person_id INT NOT NULL
+CREATE TABLE instructor (
+id INT NOT NULL,
+instruments_taught VARCHAR(500) NOT NULL,
+person_id INT NOT NULL
 );
 
-ALTER TABLE Instructor ADD CONSTRAINT PK_Instructor PRIMARY KEY (id);
+ALTER TABLE instructor ADD CONSTRAINT PK_instructor PRIMARY KEY (id);
 
 
-CREATE TABLE LessonSchedule (
- id INT NOT NULL,
- startTime  TIMESTAMP NOT NULL,
- endTime TIMESTAMP NOT NULL,
- lessonPriceId INT NOT NULL,
- instructor_id INT NOT NULL
+CREATE TABLE lesson_schedule (
+id INT NOT NULL,
+start_time  TIMESTAMP NOT NULL,
+end_time TIMESTAMP NOT NULL,
+instructor_id INT NOT NULL,
+price_id INT
 );
 
-ALTER TABLE LessonSchedule ADD CONSTRAINT PK_LessonSchedule PRIMARY KEY (id);
+ALTER TABLE lesson_schedule ADD CONSTRAINT PK_lesson_schedule PRIMARY KEY (id);
 
 
 CREATE TABLE person_phone (
- person_id INT NOT NULL,
- phone_id INT NOT NULL
+person_id INT NOT NULL,
+phone_id INT NOT NULL
 );
 
 ALTER TABLE person_phone ADD CONSTRAINT PK_person_phone PRIMARY KEY (person_id,phone_id);
 
 
-CREATE TABLE Rental (
- id INT NOT NULL,
- startDate TIMESTAMP NOT NULL,
- endDate TIMESTAMP NOT NULL,
- instrument_id INT NOT NULL,
- student_id INT NOT NULL
+CREATE TABLE rental (
+id INT NOT NULL,
+start_date TIMESTAMP NOT NULL,
+end_date TIMESTAMP NOT NULL,
+instrument_id INT NOT NULL,
+student_id INT NOT NULL
 );
 
-ALTER TABLE Rental ADD CONSTRAINT PK_Rental PRIMARY KEY (id);
+ALTER TABLE rental ADD CONSTRAINT PK_rental PRIMARY KEY (id);
 
-
-CREATE TABLE student_lessonSchedule (
- lessonId INT NOT NULL,
- student_id INT NOT NULL
+CREATE TABLE sibling (
+student_id INT NOT NULL,
+sibling_id INT NOT NULL
 );
 
-ALTER TABLE student_lessonSchedule ADD CONSTRAINT PK_student_lessonSchedule PRIMARY KEY (lessonId,student_id);
+ALTER TABLE sibling ADD CONSTRAINT PK_sibling PRIMARY KEY (student_id,sibling_id);
 
 
-CREATE TABLE LessonCapacity (
- id INT NOT NULL,
- max VARCHAR(10),
- min VARCHAR(10) NOT NULL,
- lessonId INT NOT NULL
+CREATE TABLE student_lesson_schedule (
+lesson_id INT NOT NULL,
+student_id INT NOT NULL
 );
 
-ALTER TABLE LessonCapacity ADD CONSTRAINT PK_LessonCapacity PRIMARY KEY (id);
+ALTER TABLE student_lesson_schedule ADD CONSTRAINT PK_student_lesson_schedule PRIMARY KEY (lesson_id,student_id);
 
 
-CREATE TABLE LessonGenre (
- id INT NOT NULL,
- genre  VARCHAR(50) NOT NULL,
- lessonId INT NOT NULL
+CREATE TABLE lesson_capacity (
+id INT NOT NULL,
+max VARCHAR(10),
+min VARCHAR(10) NOT NULL,
+lesson_id INT NOT NULL
 );
 
-ALTER TABLE LessonGenre ADD CONSTRAINT PK_LessonGenre PRIMARY KEY (id);
+ALTER TABLE lesson_capacity ADD CONSTRAINT PK_lesson_capacity PRIMARY KEY (id);
 
 
-CREATE TABLE LessonInstrument (
- id INT NOT NULL,
- taughtInstrument CHAR(50) NOT NULL,
- lessonId INT NOT NULL
+CREATE TABLE lesson_genre (
+id INT NOT NULL,
+genre  VARCHAR(50) NOT NULL,
+lesson_id INT NOT NULL
 );
 
-ALTER TABLE LessonInstrument ADD CONSTRAINT PK_LessonInstrument PRIMARY KEY (id);
+ALTER TABLE lesson_genre ADD CONSTRAINT PK_lesson_genre PRIMARY KEY (id);
 
 
-ALTER TABLE Student ADD CONSTRAINT FK_Student_0 FOREIGN KEY (person_id) REFERENCES Person (id);
+CREATE TABLE lesson_instrument (
+id INT NOT NULL,
+taught_instrument VARCHAR(50) NOT NULL,
+lesson_id INT NOT NULL
+);
+
+ALTER TABLE lesson_instrument ADD CONSTRAINT PK_lesson_instrument PRIMARY KEY (id);
 
 
-ALTER TABLE Instructor ADD CONSTRAINT FK_Instructor_0 FOREIGN KEY (person_id) REFERENCES Person (id);
+ALTER TABLE student ADD CONSTRAINT FK_student_0 FOREIGN KEY (person_id) REFERENCES person (id);
 
 
-ALTER TABLE LessonSchedule ADD CONSTRAINT FK_LessonSchedule_0 FOREIGN KEY (lessonPriceId) REFERENCES LessonPrice (id);
-ALTER TABLE LessonSchedule ADD CONSTRAINT FK_LessonSchedule_1 FOREIGN KEY (instructor_id) REFERENCES Instructor (id);
+ALTER TABLE instructor ADD CONSTRAINT FK_instructor_0 FOREIGN KEY (person_id) REFERENCES person (id);
 
 
-ALTER TABLE person_phone ADD CONSTRAINT FK_person_phone_0 FOREIGN KEY (person_id) REFERENCES Person (id);
-ALTER TABLE person_phone ADD CONSTRAINT FK_person_phone_1 FOREIGN KEY (phone_id) REFERENCES Phone (id);
+ALTER TABLE lesson_schedule ADD CONSTRAINT FK_lesson_schedule_0 FOREIGN KEY (instructor_id) REFERENCES instructor (id);
+ALTER TABLE lesson_schedule ADD CONSTRAINT FK_lesson_schedule_1 FOREIGN KEY (price_id) REFERENCES lesson_price (id);
 
 
-ALTER TABLE Rental ADD CONSTRAINT FK_Rental_0 FOREIGN KEY (instrument_id) REFERENCES Instrument (id);
-ALTER TABLE Rental ADD CONSTRAINT FK_Rental_1 FOREIGN KEY (student_id) REFERENCES Student (id);
+ALTER TABLE person_phone ADD CONSTRAINT FK_person_phone_0 FOREIGN KEY (person_id) REFERENCES person (id);
+ALTER TABLE person_phone ADD CONSTRAINT FK_person_phone_1 FOREIGN KEY (phone_id) REFERENCES phone (id);
 
 
-ALTER TABLE student_lessonSchedule ADD CONSTRAINT FK_student_lessonSchedule_0 FOREIGN KEY (lessonId) REFERENCES LessonSchedule (id);
-ALTER TABLE student_lessonSchedule ADD CONSTRAINT FK_student_lessonSchedule_1 FOREIGN KEY (student_id) REFERENCES Student (id);
+ALTER TABLE rental ADD CONSTRAINT FK_rental_0 FOREIGN KEY (instrument_id) REFERENCES instrument_stock (id);
+ALTER TABLE rental ADD CONSTRAINT FK_rental_1 FOREIGN KEY (student_id) REFERENCES student (id);
 
 
-ALTER TABLE LessonCapacity ADD CONSTRAINT FK_LessonCapacity_0 FOREIGN KEY (lessonId) REFERENCES LessonSchedule (id);
+ALTER TABLE sibling ADD CONSTRAINT FK_sibling_0 FOREIGN KEY (student_id) REFERENCES student (id);
+ALTER TABLE sibling ADD CONSTRAINT FK_sibling_1 FOREIGN KEY (sibling_id) REFERENCES student (id);
 
 
-ALTER TABLE LessonGenre ADD CONSTRAINT FK_LessonGenre_0 FOREIGN KEY (lessonId) REFERENCES LessonSchedule (id);
+ALTER TABLE student_lesson_schedule ADD CONSTRAINT FK_student_lesson_schedule_0 FOREIGN KEY (lesson_id) REFERENCES lesson_schedule (id);
+ALTER TABLE student_lesson_schedule ADD CONSTRAINT FK_student_lesson_schedule_1 FOREIGN KEY (student_id) REFERENCES student (id);
 
 
-ALTER TABLE LessonInstrument ADD CONSTRAINT FK_LessonInstrument_0 FOREIGN KEY (lessonId) REFERENCES LessonSchedule (id);
+ALTER TABLE lesson_capacity ADD CONSTRAINT FK_lesson_capacity_0 FOREIGN KEY (lesson_id) REFERENCES lesson_schedule (id);
+
+
+ALTER TABLE lesson_genre ADD CONSTRAINT FK_lesson_genre_0 FOREIGN KEY (lesson_id) REFERENCES lesson_schedule (id);
+
+
+ALTER TABLE lesson_instrument ADD CONSTRAINT FK_lesson_instrument_0 FOREIGN KEY (lesson_id) REFERENCES lesson_schedule (id);
 
 
